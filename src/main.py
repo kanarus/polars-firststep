15b23617a3a08ed8bcded014989635feba90a42d
kanarus <kanarus786@gmail.com>
2025-11-03 04:01:42 +0900

    initial commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import polars
+
+
+def main():
+    df = (
+        polars.scan_csv("./data/webchamame_20251102172307.csv")
+        .filter(polars.col("品詞").str.contains("名詞"))
+        .group_by(polars.col("書字形（＝表層形）"))
+        .len(name="count")
+        .sort(by=polars.col("count"), descending=True)
+        .collect()
+    )
+    for [wordform, count] in df.head(10).iter_rows():
+        print(f"A noun '{wordform}' is used {count} times")
+
+
+if __name__ == "__main__":
+    main()
